40ffdd221d5244a515c2d1f0d9018f148064c981
thierryjeh <thierry.jeh@gmail.com>
2020-05-26 01:07:54 +0200

    ..

--- script.js
+++ script.js
@@ -16,3 +16,28 @@ const navbarHeader = document.getElementById("navbarHeader");
 hamburgerMenu.addEventListener("click", (event) => {
   navbarHeader.classList.toggle("collapse");
 });
+
+
+
+
+// Fonctionnalité 3 :
+
+let btnFirstCard = document.querySelector('.card .btn-outline-secondary');
+let textFirstCard = document.querySelector('.card-text');
+
+btnFirstCard.addEventListener('click', function() {
+  textFirstCard.style.color = 'red';
+});
+
+// Fonctionnalité 4 :
+
+let btnSecondCard = document.querySelectorAll('.card')[1].querySelector('.btn-outline-secondary');
+let textSecondCard = document.querySelectorAll('.card')[1].querySelector('.card-text');
+
+btnSecondCard.addEventListener('click', function() {
+  if (textSecondCard.style.color === 'green') {
+    textSecondCard.style.color = 'black';
+  } else {
+    textSecondCard.style.color = 'green';
+  }
+})
