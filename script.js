f8d8557d6b47cd7d3cd821a7ecf7d3c396cfca2e
thierryjeh <thierry.jeh@gmail.com>
2020-05-26 00:45:17 +0200

    ..

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ footer.addEventListener("click", (event) => {
   console.log(`clic n° ${x}`)
   x++
 });
+
+
+
+// Fonctionnalité 2
+
+const hamburgerMenu = document.querySelector(".navbar-toggler");
+const navbarHeader = document.getElementById("navbarHeader");
+hamburgerMenu.addEventListener("click", (event) => {
+  navbarHeader.classList.toggle("collapse");
+});
